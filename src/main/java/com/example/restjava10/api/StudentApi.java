312f2f5bb823dc412969960b8e9b313c517347ba
user <akumusaeva01@gmail.com>
2023-09-04 18:29:33 +0300

    commit

--- src/main/java/com/example/restjava10/api/StudentApi.java
+++ src/main/java/com/example/restjava10/api/StudentApi.java
@@ -6,7 +6,9 @@ import com.example.restjava10.dto.StudentRequestRecord;
 import com.example.restjava10.dto.StudentResponse;
 import com.example.restjava10.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -16,29 +18,35 @@ public class StudentApi {
 
     private final StudentService studentService;
 
+    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")//or @PermitAll
     @GetMapping
-    public List<StudentResponse> getAllStudents(){
+    public List<StudentResponse> getAllStudents() {
         return studentService.getAllStudents();
     }
 
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
-    public SimpleResponse saveStudent(@RequestBody StudentRequestRecord studentRequest){
+    public SimpleResponse saveStudent(@RequestBody StudentRequestRecord studentRequest) {
         return studentService.saveStudent(studentRequest);
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN','INSTRUCTOR')")
     @GetMapping("/{studentId}")
-    public StudentResponse getById(@PathVariable Long studentId){
+    public StudentResponse getById(@PathVariable Long studentId) {
         return studentService.getStudentById(studentId);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{studentId}")
     public SimpleResponse updateStudent(@PathVariable Long studentId,
-                                         @RequestBody StudentRequest studentRequest){
-        return studentService.updateStudent(studentId,studentRequest);
+                                        @RequestBody StudentRequest studentRequest) {
+        return studentService.updateStudent(studentId, studentRequest);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{studentId}")
-    public SimpleResponse deleteStudent(@PathVariable Long studentId){
+    public SimpleResponse deleteStudent(@PathVariable Long studentId) {
         return studentService.deleteStudent(studentId);
     }
 
